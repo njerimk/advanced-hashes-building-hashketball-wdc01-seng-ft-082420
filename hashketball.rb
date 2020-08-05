b593aa83399114bb57f49bf0c7a32af8aa900005
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-08-05 14:43:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash(filepath)
       :colors => ["Black", "White"],
       :players =>[{:player_name => "Alan Anderson",:number => 1, :shoe => 1, :points => 2,
       :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}, 
-      {:player_name => " ",:number => 1, :shoe => 1, :points => 2,
+      {:player_name => "Reggie Evans ",:number => 1, :shoe => 1, :points => 2,
       :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}, 
       {:player_name => "",:number => 1, :shoe => 1, :points => 2,
       :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}, 
