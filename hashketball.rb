5a498a97809df4ba8de40480957f6c27251202f3
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-08-05 14:47:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,15 +3,16 @@ def game_hash(filepath)
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players =>[{:player_name => "Alan Anderson",:number => 1, :shoe => 1, :points => 2,
+      :players =>[{:player_name => "Alan Anderson",:number => 0, :shoe => 16, :points => 22,
+      :rebounds => 12,:assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
+      {:player_name => "Reggie Evans ",:number => 30, :shoe => 14, :points => 12,
+      :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, 
+      :slam_dunks => 7}, 
+      {:player_name => "Brook Lopez",:number => 1, :shoe => 1, :points => 2,
       :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}, 
-      {:player_name => "Reggie Evans ",:number => 1, :shoe => 1, :points => 2,
-      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}, 
-      {:player_name => "",:number => 1, :shoe => 1, :points => 2,
-      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}, 
-      {:player_name => "",:number => 1, :shoe => 1, :points => 2,
+      {:player_name => "Mason Plumlee",:number => 1, :shoe => 1, :points => 2,
       :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1},
-      {:player_name => "",:number => 1, :shoe => 1, :points => 2,
+      {:player_name => "Jason Terry",:number => 1, :shoe => 1, :points => 2,
       :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}]
       
       
