c7382c9cd8116683013cce3a3d947c777965078c
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-08-05 14:34:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,13 +4,15 @@ def game_hash(filepath)
       :team_name => "",
       :colors => [],
       :players =>[{:player_name => "",:number => 1, :shoe => 1, :points => 2,
-      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1 
-        
-      }, {}, {}]
+      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1 }, {}, {}]
       
       
     },
     :away => {
+      team_name => "",
+      :colors => [],
+      :players =>[{:player_name => "",:number => 1, :shoe => 1, :points => 2,
+      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1 }, {}, {}]
       
     }
     
