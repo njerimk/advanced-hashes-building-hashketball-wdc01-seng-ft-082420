4bd4c077446a0f6ba60c23bde1033311f688a002
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-08-05 14:41:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,10 @@ def game_hash(filepath)
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players =>[{:player_name => "",:number => 1, :shoe => 1, :points => 2,
-      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1 }, {}, {}]
+      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}, {:player_name => "",:number => 1, :shoe => 1, :points => 2,
+      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}, {:player_name => "",:number => 1, :shoe => 1, :points => 2,
+      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}, {:player_name => "",:number => 1, :shoe => 1, :points => 2,
+      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}]
       
       
     },
@@ -12,7 +15,10 @@ def game_hash(filepath)
       team_name => "",
       :colors => [],
       :players =>[{:player_name => "",:number => 1, :shoe => 1, :points => 2,
-      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1 }, {}, {}]
+      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1 }, {:player_name => "",:number => 1, :shoe => 1, :points => 2,
+      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}, {:player_name => "",:number => 1, :shoe => 1, :points => 2,
+      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}, {:player_name => "",:number => 1, :shoe => 1, :points => 2,
+      :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}]
       
     }
     
