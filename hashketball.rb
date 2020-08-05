e703c1f3241469904d07fd634dbb1be1b33ee720
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-08-05 14:23:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,7 @@ def game_hash(filepath)
   filepath = hashketball_spec.rb
 new_hash = {}
   new_result = new_hash.map do |key1, key2| 
-    if new_hash = key1!
-      key1 = :home
+    puts key1 = new_hash
     end
   end
   puts new_hash
