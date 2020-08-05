5654c38ef8c32d562be1df14b28ca53d7b4806d8
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-08-05 14:50:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -19,8 +19,8 @@ def game_hash(filepath)
     },
     :away => {
       team_name => "Charlotte Hornets",
-      :colors => ["turquoise"],
-      :players =>[{:player_name => "",:number => 1, :shoe => 1, :points => 2,
+      :colors => ["Turquoise", "Purple"],
+      :players =>[{:player_name => "Jeff Adrien",:number => 1, :shoe => 1, :points => 2,
       :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1 }, 
       {:player_name => "",:number => 1, :shoe => 1, :points => 2,
       :rebounds => 1,:assists => 1, :steals => 2, :blocks => 2, :slam_dunks => 1}, 
